ed2fb717a271ecb72ffd8a52c76ec0b5e37918b5
FBQ1 <791140017@qq.com>
2018-08-18 19:08:13 +0800

    second change

---
+++ 井字棋/Assets/ComputerAI.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace Assets
+{
+  
+    public class ComputerAI : MonoBehaviour
+    {
+        public void Start()
+        {
+
+        }
+        public int x = -1, y = -1;
+        public void Step1()
+        {
+            
+            Game r = new Game();
+            List<int> row = new List<int>();
+            List<int> col = new List<int>();
+            int count1 = 0;
+            int result = r.check();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Game.state[i, j] == 0 && result == 0)
+                    {
+                        row.Add(i);
+                        col.Add(j);
+                        count1++;
+                    }
+                }
+            }
+            if (count1 == 0)
+            {
+                x = -1; y = -1;
+                return;
+            }
+            System.Random ran = new System.Random();
+            if (result == 0)
+            {
+                int index = ran.Next(0, count1);
+                x = row[index];
+                y = col[index];
+                return;
+            }
+        }
+        public void Step2()
+        {
+            Game a = new Game();
+            System.Random rd = new System.Random();
+            do
+            {
+                int i = rd.Next(1, 5);
+                if (Game.state[0, 1] != 0 && Game.state[1, 0] != 0 && Game.state[1, 2] != 0 && Game.state[2, 1] != 0)
+                    break;
+                x = (2 * i - 1) / 3; y = (2 * i - 1) % 3;
+            } while (Game.state[x, y] != 0);
+        }
+        public void Step3()
+        {
+            Game a = new Game();
+            if (Game.state[1, 1] == 0)
+            { x = 1; y = 1; }
+            else
+            {
+                System.Random rd = new System.Random();
+                do
+                {
+                    int i = rd.Next(1, 10);
+                    int j = rd.Next(1, 10);
+                    if (Game.state[0, 0] != 0 && Game.state[0, 2] != 0 && Game.state[2, 0] != 0 && Game.state[2, 2] != 0)
+                        break;
+                    x = i * 2 % 4; y = j * 2 % 4;
+                } while (Game.state[x, y] != 0);
+            }
+
+        }
+        public void Step4()
+        {
+            int r = 4, s = 4, v = 1;
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    if (Game.state[m, n] == 0 && (Game.state[m, 0] == 1 || Game.state[m, 1] == 1 || Game.state[m, 2] == 1) && (Game.state[0, n] == 1 || Game.state[1, n] == 1 || Game.state[2, n] == 1) && (Game.state[m, 0] != 2 && Game.state[m, 1] != 2 && Game.state[m, 2] != 2 && Game.state[0, n] != 2 && Game.state[1, n] != 2 && Game.state[2, n] != 2))
+                    {
+                        if (r == 4 && s == 4)
+                        {
+                            r = m;
+                            s = n;
+                            v = 2;
+
+                        }
+                        else
+                        {
+                            r = x;
+                            s = y;
+                        }
+                    }
+                }
+            }
+            if (v == 2)
+            {
+                x = r;
+                y = s;
+            }
+        }
+        public void Step5()
+        {
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    if (Game.state[m, n] == 0 && (Game.state[m, 0] == 2 || Game.state[m, 1] == 2 || Game.state[m, 2] == 2) && (Game.state[0, n] == 2 || Game.state[1, n] == 2 || Game.state[2, n] == 2) && (Game.state[m, 0] != 1 && Game.state[m, 1] != 1 && Game.state[m, 2] != 1 && Game.state[0, n] != 1 && Game.state[1, n] != 1 && Game.state[2, n] != 1))
+                    {
+                        x = m;
+                        y = n;
+                    }
+                }
+            }
+        }
+
+        public void Step6(int k)
+        {
+            Game a = new Game();
+            //closs
+            if (Game.state[1, 1] == k)
+            {
+                if (Game.state[0, 0] == k && Game.state[2, 2] == 0)
+                {
+                    x = 2; y = 2;
+                }
+                else if (Game.state[2, 2] == k && Game.state[0, 0] == 0)
+                {
+                    x = 0; y = 0;
+                }
+                else if (Game.state[0, 2] == k && Game.state[2, 0] == 0)
+                {
+                    x = 2; y = 0;
+                }
+                else if (Game.state[2, 0] == k && Game.state[0, 2] == 0)
+                {
+                    x = 0; y = 2;
+                }
+            }
+            //col
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 1; j < 3; j++)
+                {
+                    if (Game.state[i, j] == k)
+                    {
+                        if (Game.state[i, (j + 1) % 3] == k && Game.state[i, (j - 1) % 3] == 0)
+                        {
+                            x = i; y = (j - 1) % 3;
+                        }
+
+                        if (Game.state[i, (j - 1) % 3] == k && Game.state[i, (j + 1) % 3] == 0)
+                        {
+                            x = i; y = (j + 1) % 3;
+                        }
+                    }
+                }
+            }
+            //cow
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 1; j < 3; j++)
+                {
+                    if (Game.state[j, i] == k)
+                    {
+                        if (Game.state[(j + 1) % 3, i] == k && Game.state[(j - 1) % 3, i] == 0)
+                        {
+                            x = (j - 1) % 3; y = i;
+                        }
+                        if (Game.state[(j - 1) % 3, i] == k && Game.state[(j + 1) % 3, i] == 0)
+                        {
+                            x = (j + 1) % 3; y = i;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 井字棋/Assets/Game.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UnityEngine;
+using Assets;
+
+public  class Game : MonoBehaviour
+{
+
+    public static int[,] state = new int[3, 3];
+    public int count = 0;
+    public int A = 0;
+    public int B = 1;
+    public int C = 2;
+    public int D = 0;
+    public int k;
+    public int a, b, c, d;
+    public static Game  onlineiplist = new Game();
+    public void Start()
+    {
+        reset();
+    }  
+    public void OnGUI()
+    {
+        ComputerAI r = new ComputerAI();
+        GUIStyle fontStyle = new GUIStyle();
+        fontStyle.normal.background = null;
+        fontStyle.normal.textColor = new Color(1, 0, 0);
+        fontStyle.fontSize = 24;
+        GUI.backgroundColor = Color.red;
+        int result = check();
+        if (GUI.Button(new Rect(385, 200, 100, 50), "电脑先手")&&state[1,1]==0)
+        {
+            state[1, 1] = 2;
+        }
+        if (GUI.Button(new Rect(275, 200, 100, 50), "Reset"))
+            reset();
+        
+        if (GUI.Button(new Rect(330, 300, 100, 50), "悔棋") && count != 0 && result == 0)
+        {
+            state[a, b] = 0;
+            state[c, d] = 0;
+        }
+
+        if (result == 1)
+        {
+            GUI.Label(new Rect(325, 170, 100, 50), " 你赢了!", fontStyle);
+            A = 1;
+            output();
+        }
+        else if (result == 2)
+        {
+            GUI.Label(new Rect(325, 170, 100, 50), "菜鸟，你输了", fontStyle);
+            A = 2;
+           output();
+        }     
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (state[i, j] == 1) 
+                    GUI.Button(new Rect(300 + i * 50, j * 50, 50, 50), "O");
+                if (state[i, j] == 2)
+                    GUI.Button(new Rect(300 + i * 50, j * 50, 50, 50), "X");
+                if (GUI.Button(new Rect(300 + i * 50, j * 50, 50, 50), ""))
+                {
+                    if (result == 0)
+                    {
+                        a = i; b = j;
+                        state[i, j] = 1;
+                        result = check();
+                        r.Step1();
+
+                        if ((state[0, 2] == 1 && state[2, 0] == 1) || (state[0, 0] == 1 && state[2, 2] == 1))
+                            r.Step2();
+                        else
+                            r.Step3();
+                        r.Step4();
+                        r.Step5();
+                        r.Step6(1);
+                        r.Step6(2);
+                        if (r.x != -1 && r.y != -1 && state[r.x, r.y] == 0 && result == 0)
+                        {
+                            state[r.x, r.y] = 2;
+                            c = r.x; d = r.y;
+                        }
+                    }
+                }
+                                                        
+            }
+        }
+        count=0;
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j=0; j < 3; j++)
+            {
+                if (state[i, j] == 0)
+                    count++;
+            }
+        }
+        if (result == 0 && count == 0)
+        {
+            GUI.Label(new Rect(315, 170, 100, 50), "平局", fontStyle);
+            output();
+        }           
+    }
+
+    public void output()
+    { 
+        string result1 = @"D:\对战结果.txt";
+        FileStream fs = new FileStream(result1, FileMode.Append, FileAccess.Write, FileShare.Write);
+        StreamWriter wr = null;
+        wr = new StreamWriter(fs);
+        if (A == 1 && B == 1)
+        {
+            wr.WriteLine("你赢了!");
+            wr.Close();
+            B++;
+        }
+        else if (A == 2 && C == 2)
+        {
+            wr.WriteLine("菜鸟，你输了");
+            wr.Close();
+            C++;
+        }
+        if (A == 0 && count == 0 && D == 0)
+        {
+            wr.WriteLine("平局");
+            wr.Close();
+            D++;
+        }
+    }
+    public int check()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (state[i, 0] != 0 && state[i, 0] == state[i, 1] && state[i, 1] == state[i, 2])
+            {
+                return state[i, 0];
+            }
+            if (state[0, i] != 0 && state[0, i] == state[1, i] && state[1, i] == state[2, i])
+            {
+                return state[0, i];
+            }
+        }
+        if (state[1, 1] != 0 && (state[0, 0] == state[1, 1] && state[1, 1] == state[2, 2] || state[0, 2] == state[1, 1] && state[1, 1] == state[2, 0]))
+        {
+            return state[1, 1];
+        }
+        return 0;
+    }
+    public void reset()
+    {
+        count = 0;
+        A = 0;
+        B = 1;
+        C = 2;
+        D = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                state[i, j] = 0;
+            }
+        }
+    }
+  
+    void Update()
+    {
+
+    }
+}
